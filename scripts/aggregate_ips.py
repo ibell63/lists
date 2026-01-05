477fc4a804872e630994322e71318c852453d346
ibell63 <25824529+ibell63@users.noreply.github.com>
2026-01-04 21:00:39 -0500

    Update aggregate_ips.py
    Signed-off-by: ibell63 <25824529+ibell63@users.noreply.github.com>

--- scripts/aggregate_ips.py
+++ scripts/aggregate_ips.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 SOURCES = [
     "https://iplists.firehol.org/files/tor_exits_30d.ipset",
-    "https://iplists.firehol.org/files/tor_exits_1d.ipset",
+    "https://iplists.firehol.org/files/tor_exits_7d.ipset",
     "https://iplists.firehol.org/files/tor_exits.ipset",
     "https://iplists.firehol.org/files/socks_proxy_30d.ipset",
     "https://iplists.firehol.org/files/socks_proxy_7d.ipset",
