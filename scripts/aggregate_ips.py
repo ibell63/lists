b3e5eeac0eed526f44661a8982fb5d0fe2d4837b
ibell63 <25824529+ibell63@users.noreply.github.com>
2026-01-06 09:28:19 -0500

    Update aggregate_ips.py
    Signed-off-by: ibell63 <25824529+ibell63@users.noreply.github.com>

--- scripts/aggregate_ips.py
+++ scripts/aggregate_ips.py
@@ -46,6 +46,7 @@ SOURCES = [
     "https://iplists.firehol.org/files/myip.ipset",
     "https://iplists.firehol.org/files/firehol_proxies.netset",
     "https://iplists.firehol.org/files/firehol_level2.netset",
+    "https://raw.githubusercontent.com/stamparm/ipsum/master/levels/1.txt",
     "https://raw.githubusercontent.com/stamparm/ipsum/master/levels/2.txt",
     "https://raw.githubusercontent.com/stamparm/ipsum/master/levels/3.txt",
     "https://raw.githubusercontent.com/stamparm/ipsum/master/levels/4.txt",
